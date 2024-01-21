1b641fc2e2f73f9f98467761d2fb1c3b014a3ad7
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-01-20 23:00:28 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ SERVICE_ARN="arn:aws:apprunner:region:account:service/maxiplux-python-2024"
 
 if aws apprunner describe-service --service-arn $SERVICE_ARN > /dev/null 2>&1; then
     echo "Service exists, updating..."
-    aws apprunner update-service --service-arn $SERVICE_ARN --cli-input-json file://update-service.json
+    mkdir /app
+    chmod 666 /app
+    aws apprunner update-service --service-arn $SERVICE_ARN --cli-input-json file:///app/update-service.json
 else
     echo "Service does not exist, creating..."
-    aws apprunner create-service --cli-input-json file://create-service.json
+    aws apprunner create-service --cli-input-json file:///app/create-service.json
 fi
